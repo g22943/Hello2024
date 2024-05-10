7d526b6b00e1708f61b1a25c980f2a79ff7295d0
pearno <g22943ur@gm.tsuda.ac.jp>
2024-05-10 16:06:02 +0900

    こんにちわっぴー

--- src/HelloSample.java
+++ src/HelloSample.java
@@ -4,6 +4,7 @@ public class HelloSample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!!!!");
+		System.out.println("Have a nice day!");
 
 	}
 
